1944f4e098ac785311f4d5c96774130305129b3d
Akira Maruoka <akmaru0266@gmail.com>
2023-02-26 09:14:14 +0000

    [Test] Fixed GoogleTest.cmake

--- cmake/module/GoogleTest.cmake
+++ cmake/module/GoogleTest.cmake
@@ -1,11 +1,12 @@
 # Google Test
-set(GTEST_VERSION "v1.13.0" CACHE STRING "Version of google test")
+set(GTEST_VERSION "v1.13.0" CACHE STRING "Version of GoogleTest")
 
 include(FetchContent)
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG ${GTEST_VERSION})
+  GIT_TAG ${GTEST_VERSION}
+)
 
 ## For Windows: Prevent overriding the parent project's compiler/linker settings
 if(WIN32)
